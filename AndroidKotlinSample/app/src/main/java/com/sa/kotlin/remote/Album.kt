1d8c62f156127feb417e59561ab04c5c20da034a
sujitacharay2005 <sujitacharya2005@gmail.com>
2019-12-01 15:38:32 +0530

    Fetching data from JsonPlaceHolder by using Retrofit

---
+++ AndroidKotlinSample/app/src/main/java/com/sa/kotlin/remote/Album.kt
@@ -0,0 +1,7 @@
+package com.sa.kotlin.remote
+
+data class Album (
+    val userId:Int,
+    val id:Int,
+    val title:String
+)
---
+++ AndroidKotlinSample/app/src/main/java/com/sa/kotlin/remote/Photo.kt
@@ -0,0 +1,9 @@
+package com.sa.kotlin.remote
+
+class Photo (
+    val albumId:Int,
+    val id:Int,
+    val title:String,
+    val url:String,
+    val thumbnailUrl:String
+)
