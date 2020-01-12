f2b531a8bac92b130bc0fedd648cd858eb5ff27a
sujitacharay2005 <sujitacharya2005@gmail.com>
2020-01-12 16:14:09 +0530

    Retrofit2

--- AchitecuturalComponents/app/src/main/java/com/sa/achitecuturalcomponents/network/JsonPlaceHolderService.kt
+++ AchitecuturalComponents/app/src/main/java/com/sa/achitecuturalcomponents/network/JsonPlaceHolderService.kt
@@ -1,9 +1,10 @@
 package com.sa.achitecuturalcomponents.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 import retrofit2.http.GET
 
 
@@ -14,11 +15,21 @@ interface JsonPlaceHolderService {
     companion object {
         const val BASE_URL = "https://jsonplaceholder.typicode.com"
 
+        var okHttpClientBuilder = OkHttpClient.Builder()
+        var gsonFactory= GsonConverterFactory.create()
+
+
         fun create() : JsonPlaceHolderService {
+            var loggingInterceptor = HttpLoggingInterceptor();
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BASIC
+
+            okHttpClientBuilder.addInterceptor(loggingInterceptor)
             val retrofit = Retrofit.Builder()
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(gsonFactory)
                 .baseUrl(BASE_URL)
+                .client(okHttpClientBuilder.build())
                 .build();
+
             return retrofit.create(JsonPlaceHolderService::class.java)
         }
     }
