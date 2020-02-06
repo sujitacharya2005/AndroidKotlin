ff9a285c56c7b897ec2a57860243222a27fe39d2
sujitacharay2005 <sujitacharya2005@gmail.com>
2020-02-07 01:33:58 +0530

    GridLayoutManager RecyclerView

--- AchitecuturalComponents/app/src/main/java/com/sa/achitecuturalcomponents/network/paging/AlbumActivity.kt
+++ AchitecuturalComponents/app/src/main/java/com/sa/achitecuturalcomponents/network/paging/AlbumActivity.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.paging.PagedList
+import androidx.recyclerview.widget.GridLayoutManager
 import com.sa.achitecuturalcomponents.R
 import kotlinx.android.synthetic.main.activity_album.*
 
@@ -16,6 +17,7 @@ class AlbumActivity : AppCompatActivity() {
         setContentView(R.layout.activity_album)
         val adapter = AlbumPagedListAdapter()
         recyclerView.adapter = adapter
+        recyclerView.layoutManager = GridLayoutManager(this, 3);
         val albumViewModel:AlbumViewModel = ViewModelProviders.of(this).get(AlbumViewModel::class.java)
         albumViewModel.liveData.observe(this, Observer { pagedList->adapter.submitList(pagedList) })
 
--- AchitecuturalComponents/app/src/main/java/com/sa/achitecuturalcomponents/network/paging/AlbumPagedListAdapter.kt
+++ AchitecuturalComponents/app/src/main/java/com/sa/achitecuturalcomponents/network/paging/AlbumPagedListAdapter.kt
@@ -3,11 +3,16 @@ package com.sa.achitecuturalcomponents.network.paging
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.jakewharton.picasso.OkHttp3Downloader
 import com.sa.achitecuturalcomponents.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 class AlbumPagedListAdapter : PagedListAdapter<Photoos, AlbumPagedListAdapter.AlbumViewHoler>(diffCallBack) {
 
@@ -15,9 +20,18 @@ class AlbumPagedListAdapter : PagedListAdapter<Photoos, AlbumPagedListAdapter.Al
     class AlbumViewHoler(parent:ViewGroup) : RecyclerView.ViewHolder(
         LayoutInflater.from(parent.context).inflate(R.layout.photo_item,  parent, false)) {
         private val titleTxtView = itemView.findViewById<TextView>(R.id.title)
+        private val thumImageView = itemView.findViewById<ImageView>(R.id.coverImage)
+        val builder =  Picasso.Builder(parent.context)
+            .downloader(OkHttp3Downloader(parent.context))
 
         fun bindTo(photoos: Photoos?) {
             titleTxtView.text = photoos?.title
+            builder.build().load(photoos?.thumbnailUrl)
+                .placeholder((R.drawable.ic_launcher_background))
+                .error(R.drawable.ic_launcher_background)
+                .into(thumImageView);
+
+            //Glide.with(itemView.context).load(photoos?.thumbnailUrl).into(thumImageView)
         }
     }
 
